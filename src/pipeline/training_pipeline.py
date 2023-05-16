f87213d2881c2531a3653de791d016cffaf24022
pritambhakta90@gmail.com <pritambhakta90@gmail.com>
2023-05-16 18:25:57 +0530

    update file

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -12,7 +12,9 @@ from src.components.model_evaluation import ModelEvaluation
 from src.components.model_pusher import ModelPusher
 
 
-if __name__=="__main__":
+
+
+def start_training_pipeline():
     try:
         #get_collection_as_dataframe(database_name="IOT_SENSOR_DATA",collection_name="SMOKE_DATA")
         training_pipeline_config = config_entity.TrainingPipelineConfig()
@@ -31,22 +33,18 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
 
 
-
         #DATA TRANSFORMATION
 
-        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation_config = config_entity.DataTransformationConfig(traing_pipeline_config=training_pipeline_config)
         data_transformation = DataTransformation(data_transformation_config=data_transformation_config,data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
 
 
-
         #Model trainer
 
-        model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer_config = config_entity.ModelTrainerConfig(traing_pipeline_config=training_pipeline_config)
         model_trainer = ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
         model_tariner_artifact = model_trainer.initiate_model_trainer()
-
-
 
 
         #Model Evaluation
@@ -59,16 +57,20 @@ if __name__=="__main__":
         model_eval_artifact = model_evaluation.initiate_model_evaluation()
 
 
-          #Model Pusher
+        #Model Pusher
 
         model_pusher_config = config_entity.ModelPusherConfig(training_pipeline_config=training_pipeline_config)
         model_pusher = ModelPusher(model_pusher_config=model_pusher_config,
                                    data_transformation_artifact=data_transformation_artifact,
                                    model_trainer_artifact=model_tariner_artifact)
         model_pusher_artifact = model_pusher.initiate_model_pusher()
+    
 
-        
 
 
     except Exception as e:
-        raise CyberException(e,sys)
+        CyberException(e,sys)
+
+
+
+
